136cc8ff9a41cb25d2391a2b3b53e4bb23d14843
Me-gumin <2055809390@qq.com>
2025-09-08 12:45:27 +0800

    fix some bug

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from .ComfyUI_Nudenet.Nudenet import (
+    NODE_CLASS_MAPPINGS as Nudenet_NODE_CLASS_MAPPINGS,
+    NODE_DISPLAY_NAME_MAPPINGS as Nudenet_NODE_DISPLAY_NAME_MAPPINGS,
+)
+from .MaskOverlayNode.mask_overlay_node import MaskOverlayNode
+from .model_precision_converter.nodes import (
+    NODE_CLASS_MAPPINGS as Precision_NODE_CLASS_MAPPINGS,
+    NODE_DISPLAY_NAME_MAPPINGS as Precision_NODE_DISPLAY_NAME_MAPPINGS,
+)
+
+# 合并映射
+NODE_CLASS_MAPPINGS = {
+    **Nudenet_NODE_CLASS_MAPPINGS,
+    **Precision_NODE_CLASS_MAPPINGS,
+    "MaskOverlayNode": MaskOverlayNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    **Nudenet_NODE_DISPLAY_NAME_MAPPINGS,
+    **Precision_NODE_DISPLAY_NAME_MAPPINGS,
+    "MaskOverlayNode": "Mask Overlay Node",
+}
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
